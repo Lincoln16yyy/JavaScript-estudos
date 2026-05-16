e343c57d6bd18f0977e2a025b22b7f54c4699678
Lincoln16yyy <kakuoriginal3@outlook.com>
2026-05-16 09:55:55 -0300

    feat: aula 1

---
+++ aula1.js
@@ -0,0 +1,44 @@
+const list = [
+    {
+        name: 'João',
+        age: 30
+
+    },
+    {
+        name: 'Maria',
+        age: 25
+    },
+    {
+        name: 'Pedro',
+        age: 35
+    },
+    {
+        name: 'Ana',
+        age: 28
+    },
+    {
+        name: 'Carlos',
+        age: 32
+    },
+    {
+        name: 'Luiza',
+        age: 27
+    },
+    {
+        name: 'Rafael',
+        age: 29
+    },
+    {
+        name: 'Fernanda',
+        age: 31
+    },
+    {
+        name: 'Gustavo',
+        age: 26
+    }
+];
+
+console.log(list.map(item => item.name + ' - ' + item.age));
+
+        
+    
